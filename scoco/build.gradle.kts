9cc3a4faba2604eb140cef95ab434ab42ceb83c9
Vaios Tsitsonis <vaios.tsitsonis@theblueground.com>
2019-12-15 19:16:50 +0200

    Add testing dependencies

--- scoco/build.gradle.kts
+++ scoco/build.gradle.kts
@@ -33,4 +33,6 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.3")
     testImplementation("junit:junit:4.12")
+    testImplementation("io.mockk:mockk:1.9.3")
+    testImplementation("org.amshove.kluent:kluent:1.58")
 }
